0b844861e41c91388d033fd84d1950668642f9a9
meron <merontty1@gmail.com>
2026-02-07 11:45:06 +0300

    P

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,9 @@
 function MainContent() {
     return (
-<main>
+<main  style={{
+        padding: '20px',
+        backgroundColor: '#f5f5f5'
+      }}>
   <p>I love to visit New York, Paris, and Tokyo.</p>
 </main>
     )
