4a6d442f6c8ba7d5a107b614031ab571fc06f7a0
meron <merontty1@gmail.com>
2026-02-12 00:14:36 +0300

    P

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './component/Navbar';
+import Navbar from './Navbar';
 
 import Home from './pages/Home';
 import About from './pages/About';
