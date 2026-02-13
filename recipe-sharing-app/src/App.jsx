1d2fca48c65f5da16cdbcd937ac257558b0eed6f
meron <merontty1@gmail.com>
2026-02-13 20:40:46 +0300

    P

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import AddRecipeForm from './componetns/AddRecipeForm';
-import RecipeList from './componetns/RecipeList';
+import AddRecipeForm from './Component/AddRecipeForm';
+import RecipeList from './Component/RecipeList';
 function App() {
   const [count, setCount] = useState(0)
 
