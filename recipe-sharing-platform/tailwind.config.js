0bfe4fec05a17ee136090af8a5360fb5a30ac9fb
meron <merontty1@gmail.com>
2026-02-14 08:02:29 +0300

    P

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    "./public/index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: false,
   theme: {
     extend: {},
   },
