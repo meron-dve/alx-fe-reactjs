ffa074f530b41155114dd0963de8c2b678eb1fa4
meron <merontty1@gmail.com>
2026-02-15 18:07:40 +0300

    P

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,36 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-  import HomePage from "./components/HomePage";
-  import { Routes, Route } from 'react-router-dom';
-import RecipeDetail from './components/RecipeDetail';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
-
-
-
-  
-
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">
-        Tailwind is Working 🚀
-        
-      </h1>
-       <HomePage />;
-     
-    
+    <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
       </Routes>
-    
-
-    </div>
+    </Router>
   );
 }
-
 
 export default App;
