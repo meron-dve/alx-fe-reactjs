6e8bc336121910564b8ed77d280e2f822208fd86
meron <merontty1@gmail.com>
2026-02-20 19:17:46 +0300

    P

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,12 +1,14 @@
 import SearchBar from "./components/SearchBar";
 import RecipeList from "./components/RecipeList";
 import SearchBar from "./components/SearchBar";
+import AddRecipeForm from "./components/AddRecipeForm";
 function App() {
   return (
     <div className="max-w-6xl mx-auto px-4 py-10">
       <h1 className="text-3xl font-bold text-center mb-6">
         Recipe Sharing App
       </h1>
+<AddRecipeForm />
 
       <SearchBar />
       <RecipeList />
