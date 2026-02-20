9f27926a99a832dbdefef7712f83b400dfb8af5e
meron <merontty1@gmail.com>
2026-02-20 22:39:02 +0300

    P

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,19 +1,40 @@
 import SearchBar from "./components/SearchBar";
 import RecipeList from "./components/RecipeList";
 import SearchBar from "./components/SearchBar";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 import AddRecipeForm from "./components/AddRecipeForm";
 function App() {
   return (
-    <div className="max-w-6xl mx-auto px-4 py-10">
-      <h1 className="text-3xl font-bold text-center mb-6">
-        Recipe Sharing App
-      </h1>
-<AddRecipeForm />
+   
 
-      <SearchBar />
-      <RecipeList />
-    </div>
+
+        <Routes>
+          {/* Main page: add recipes + search + list */}
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <SearchBar />
+                <RecipeList />
+              </>
+            }
+          />
+
+          {/* Recipe detail page */}
+          <Route
+            path="/recipe/:id"
+            element={<RecipeDetails />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
   );
 }
 
