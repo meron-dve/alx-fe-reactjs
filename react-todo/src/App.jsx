d99595e4606d8fa0ff738c5c6fff72620bd3b8f3
meron <merontty1@gmail.com>
2026-02-22 19:45:38 +0300

    P

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
+  <>
+    <TodoList />
+  
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +23,7 @@ function App() {
         </a>
       </header>
     </div>
+    </>
   );
 }
 
