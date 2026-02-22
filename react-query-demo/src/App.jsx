39e0786801e9124e89ec31f55890266d1acf57d1
meron <merontty1@gmail.com>
2026-02-22 15:58:08 +0300

    P

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,20 +1,25 @@
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
-import PostsComponent from "./components /PostsComponent";
+import PostsComponent from "./components/PostsComponent";
+
+const queryClient = new QueryClient();
 
 function App() {
   const [showPosts, setShowPosts] = useState(true);
 
   return (
-    <div>
-      <h1>React Query Demo</h1>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1>React Query Demo</h1>
 
-      <button onClick={() => setShowPosts(!showPosts)}>
-        Toggle Posts
-      </button>
+        <button onClick={() => setShowPosts(!showPosts)}>
+          Toggle Posts
+        </button>
 
-      {showPosts && <PostsComponent />}
-    </div>
+        {showPosts && <PostsComponent />}
+      </div>
+    </QueryClientProvider>
   );
 }
 
