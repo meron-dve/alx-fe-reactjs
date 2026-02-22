6700fa176f5560da9fc6e782f94d48aa82302620
meron <merontty1@gmail.com>
2026-02-22 17:06:48 +0300

    P

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,46 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import BlogPost from "./pages/BlogPost";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/profile">Profile</Link> |{" "}
+        <Link to="/blog/1">Blog 1</Link> |{" "}
+        <Link to="/login">Login</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* Protected Route */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Dynamic Route */}
+        <Route path="/blog/:id" element={<BlogPost />} />
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
